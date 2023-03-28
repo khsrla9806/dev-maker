79f35be8c059d42dedf6007a6761ff0280766138
khsrla9806 <kimhunsope@naver.com>
2023-03-29 00:56:19 +0900

    feat: add Annotation for Auditing

--- src/main/java/com/fastcampus/devmaker/devmaker/DevmakerApplication.java
+++ src/main/java/com/fastcampus/devmaker/devmaker/DevmakerApplication.java
@@ -2,7 +2,9 @@ package com.fastcampus.devmaker.devmaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DevmakerApplication {
 
