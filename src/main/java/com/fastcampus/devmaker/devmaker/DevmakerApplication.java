9d568cc6a8753454a06cf8146b35c73331aaf981
khsrla9806 <kimhunsope@naver.com>
2023-03-28 19:01:42 +0900

    feat: project initial setting

---
+++ src/main/java/com/fastcampus/devmaker/devmaker/DevmakerApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.devmaker.devmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcampus/devmaker/devmaker/DevmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.devmaker.devmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
