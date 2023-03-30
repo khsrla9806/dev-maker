43261e8127d2539ee868be838caf223fdc6c1700
khsrla9806 <kimhunsope@naver.com>
2023-03-31 00:53:13 +0900

    feat: developer update를 하기위한 PUT 요청 controller 구현

--- src/main/java/com/fastcampus/devmaker/devmaker/controller/DevMakerController.java
+++ src/main/java/com/fastcampus/devmaker/devmaker/controller/DevMakerController.java
@@ -3,6 +3,7 @@ package com.fastcampus.devmaker.devmaker.controller;
 import com.fastcampus.devmaker.devmaker.dto.CreateDeveloper;
 import com.fastcampus.devmaker.devmaker.dto.DeveloperDetailDto;
 import com.fastcampus.devmaker.devmaker.dto.DeveloperDto;
+import com.fastcampus.devmaker.devmaker.dto.EditDeveloper;
 import com.fastcampus.devmaker.devmaker.service.DevMakerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,11 +28,21 @@ public class DevMakerController {
 
     @GetMapping("/developers")
     public List<DeveloperDto> getAllDevelopers() {
+        log.info("GET: /developers HTTP/1.1");
+
         return devMakerService.getAllDevelopers();
     }
 
     @GetMapping("/developers/{memberId}")
     public DeveloperDetailDto getAllDevelopers(@PathVariable String memberId) {
+        log.info("GET: /developers/:memberId HTTP/1.1");
         return devMakerService.getDeveloper(memberId);
+    }
+
+    @PutMapping("/developer/{memberId}")
+    public DeveloperDetailDto editDeveloper(@PathVariable String memberId, @Valid @RequestBody EditDeveloper.Request request) {
+        log.info("PUT: /developer/:memberId HTTP/1.1");
+
+        return devMakerService.editDeveloper(memberId, request);
     }
 }
