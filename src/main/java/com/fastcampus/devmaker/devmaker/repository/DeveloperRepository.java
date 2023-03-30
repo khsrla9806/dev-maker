8d6088a9a918dd14b895e455f451e8a16d4a23f7
khsrla9806 <kimhunsope@naver.com>
2023-03-31 01:26:09 +0900

    feat: 재직중인 개발자만 얻어올 수 있는 JPA 함수 작성

--- src/main/java/com/fastcampus/devmaker/devmaker/repository/DeveloperRepository.java
+++ src/main/java/com/fastcampus/devmaker/devmaker/repository/DeveloperRepository.java
@@ -1,12 +1,16 @@
 package com.fastcampus.devmaker.devmaker.repository;
 
 import com.fastcampus.devmaker.devmaker.entity.Developer;
+import com.fastcampus.devmaker.devmaker.type.StateCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DeveloperRepository extends JpaRepository<Developer, Long> {
     Optional<Developer> findByMemberId(String memberId);
+
+    List<Developer> findDevelopersByStateCodeEquals(StateCode stateCode);
 }
