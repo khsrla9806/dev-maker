e2d59b00901864da92ef184f239e4ea9eb007543
khsrla9806 <kimhunsope@naver.com>
2023-03-31 17:45:50 +0900

    fix: Controller 테스트 실행 시 JpaAuditing 관련 Bean을 가져오지 못하는 오류 해결

---
+++ src/main/java/com/fastcampus/devmaker/devmaker/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.fastcampus.devmaker.devmaker.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
